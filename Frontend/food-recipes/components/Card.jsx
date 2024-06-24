290f253679748b003f933d79d9c9255fc1986f33
M buda <buda.mindaugas@gmail.com>
2024-06-24 15:33:58 +0300

    created working paging  functionality

--- Frontend/food-recipes/components/Card.jsx
+++ Frontend/food-recipes/components/Card.jsx
@@ -5,19 +5,16 @@ import '../css/Card.css';
 export function Card({ photoUrl, title, text, id }) {
     return (
         <div className="card">
-           <div className="card-content">
-                <div className="img-container">
-                    <img src={photoUrl} alt='course-img' className="card-image" />
-                </div>
-                <div className="card-text">
-                    <h3 className="card-title">{title}</h3>
-                </div>
-            </div> 
-            <div className="card-button">
-                <Link to={`/recipe/${id}`}>
-                    <Button text='READ MORE'/>
-                </Link>
-            </div>
+            <Link to={`/recipe/${id}`}>
+            <div className="card-content">
+                    <div className="img-container">
+                        <img src={photoUrl} alt='course-img' className="card-image" />
+                    </div>
+                    <div className="card-text">
+                        <h3 className="card-title">{title}</h3>
+                    </div>
+                </div> 
+            </Link>
         </div>
     )
 }
--- Frontend/food-recipes/pages/Recipes.jsx
+++ Frontend/food-recipes/pages/Recipes.jsx
@@ -1,13 +1,80 @@
 import { SearchBar } from "../components/SearchBar";
 import '../css/Recipes.css';
+import { useEffect, useState } from "react";
+import { Card } from "../components/Card";
 
 export function Recipes() {
+
+    const [recipes, setRecipes] = useState([]);
+    const [error, setError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const recipesPerPage = 16;
+    
+
+    const fetchRecipes = async() => {
+        try {
+            const response = await fetch(`http://localhost:8080/recipes`);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setRecipes(data);
+            } else {
+                setError(`Error: ${response.status} ${response.statusText}`);
+            }
+        } catch (error) {
+            setError(`Error: ${error.message}`);
+        }
+    };
+
+    useEffect(() => {
+        fetchRecipes();
+    }, []);
+
+    const indexOfLastRecipe = currentPage * recipesPerPage;
+    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
+    const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const totalPages = Math.ceil(recipes.length / recipesPerPage);
+    console.log('total pagees:', totalPages);
+
+    const pageNumbersArray = Array(totalPages).fill(null);
+    console.log(pageNumbersArray)
+    const pageNumbers = pageNumbersArray.map((_, index) => index + 1);
+
+    console.log('amount of pages:', pageNumbers)
+
     return (
         <>
             <div className="recipes-wrapper">
                 <div>
                     <h1>RECIPE INDEX</h1>
                     <SearchBar className='recipes-search-bar'/>
+                </div>
+                <h2>ALL RECIPES</h2>
+                <div className="all-recipes">
+                    {currentRecipes.map(recipe => (
+                    <Card 
+                        key={recipe._id}
+                        photoUrl={recipe.photoUrl}
+                        title={recipe.title}
+                        id={recipe._id}/>
+                        
+                ))}
+                </div>
+                <div className="pagination">
+                    {pageNumbers.map(pageNumber => (
+                        <button
+                            key={pageNumber}
+                            onClick={() => handlePageChange(pageNumber)}
+                            className={currentPage === pageNumber ? 'active' : ''}
+                        >
+                            {pageNumber}
+                        </button>
+                    ))}
                 </div>
             </div>
         </>
